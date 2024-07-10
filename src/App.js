870883f69599edae71a66bb206e61073b539f7e9
imran2904 <160462268+imran2904@users.noreply.github.com>
2024-07-10 18:15:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
-import { WeatherApp } from './Components/WeatherApp/WeatherApp';
-
+import "./App.css";
+import { WeatherApp } from "./Components/WeatherApp/WeatherApp";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
-      <WeatherApp/>
+      <WeatherApp />
     </div>
   );
 }
